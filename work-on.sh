4f8a8d45b0f8365fa6fd9558f631896cab827820
Richard Flosi <richard.flosi@gmail.com>
2021-10-21 12:48:13 -0400

    Use separate error.log files, report chrome exit code when not 0, clean up output

--- work-on.sh
+++ work-on.sh
@@ -1,21 +1,32 @@
 #!/bin/sh
 
-WORKING_ON=$1; # get directory path from first command line argument
+WORKING_ON=$1; # get directory name from first command line argument
+
+# Create working directory and error log if needed
+mkdir -p ${WORKING_ON};
+touch ${WORKING_ON}/error.log;
 
 echo "Working on... ${WORKING_ON}";
+echo "Chrome error log at ./${WORKING_ON}/error.log";
 echo "Use ^C to close your browser session.";
-echo "Re-run this command to restore this session.";
-echo "Use ^Z to suspend your browser session.";
-echo "Run 'jobs' to list your background jobs.";
-echo "Run 'fg' or 'fg %[JOB_SPEC]' to restore a suspended browser session.";
 
 # Create or Restore a Chrome session based on what you are working on
-# Redirect Chrome output to work.log
+# Redirect Chrome output errors to error.log
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
-  --restore-last-session --user-data-dir="./${WORKING_ON}" \
-  2>> work.log;
+  --user-data-dir="./${WORKING_ON}" --restore-last-session \
+  2>> ./${WORKING_ON}/error.log;
 
-echo "\n";
-echo "Closing... ${WORKING_ON}";
+CHROME_EXIT=$?; # capture chrome exit code
 
-exit 0;
+echo;
+
+if [ $CHROME_EXIT -eq 0 ]
+then
+  echo "Session saved: ${WORKING_ON}";
+else
+  echo "Chrome exited with error code: ${CHROME_EXIT}. See ./${WORKING_ON}/error.log for error details.";
+fi
+
+echo "Re-run ./work-on.sh ${WORKING_ON} to restore this session.";
+
+exit $CHROME_EXIT;
